f3a9f02b62d32b6ed159f0cf3c0875f3583dd9de
ruitang <du0001@aliyun.com>
2015-05-19 22:34:04 +0800

    使用加密方式访问gravatar.com

--- wp-includes/link-template.php
+++ wp-includes/link-template.php
@@ -3601,7 +3601,7 @@ function get_avatar_data( $id_or_email, $args = null ) {
 		'r' => $args['rating'],
 	);
 
-	$url = sprintf( 'http://%d.gravatar.com/avatar/%s', $gravatar_server, $email_hash );
+	$url = sprintf( 'https://%d.gravatar.com/avatar/%s', $gravatar_server, $email_hash );
 
 	$url = add_query_arg(
 		rawurlencode_deep( array_filter( $url_args ) ),
